5bca7ef1986c80ceb78bd6904e3879fb3a8ce2ca
pramancode <praman.gupta3@gmail.com>
2022-02-14 11:10:19 +0530

    Chat with Social Signup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Button, ActivityIndicator } from 'react-native';
+import { View, Text, Button, ActivityIndicator, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from 'firebase/app';
@@ -9,50 +9,27 @@ import * as Facebook from 'expo-facebook';
 import ChatsList from './ChatsList';
 import Chat from './Chat';
 import AsyncStorageLib from '@react-native-async-storage/async-storage';
-import * as Google from 'expo-auth-session/providers/google';
+import * as GoogleSignIn from 'expo-google-sign-in';
 
-const GOOGLE_ANDROID_CLIENT_ID = process.env.GOOGLE_ANDROID_CLIENT_ID;
 const GOOGLE_EXPO_CLIENT_ID = process.env.GOOGLE_EXPO_CLIENT_ID;
 const FB_APP_ID = process.env.FB_APP_ID;
 const Stack = createNativeStackNavigator();
-//624422366265-dg6s72v7ipas3kj0rsllkh7o91qf2495.apps.googleusercontent.com
-function GoogleSignIn({ onReturn }) {
-    const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
-        {
-            clientId: GOOGLE_EXPO_CLIENT_ID,
-            androidClientId: GOOGLE_ANDROID_CLIENT_ID
-        },
-    );
-    React.useEffect(() => {
-        if (response?.type === 'success') {
-            //console.log(JSON.stringify(response));
-            const { id_token } = response.params;
-            onReturn(id_token);
-        }
-    }, [response]);
-
-    return (
-        <Button
-            onPress={() => {
-                promptAsync();
-            }}
-            title="Google Signin"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-        />
-    )
-}
 
 export default class App extends React.Component {
 
-    state = {
-        loggedIn: false,
-        isReady: false,
-        userId: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            isReady: false,
+            userId: null
+        };
+        GoogleSignIn.initAsync({
+            webClientId: GOOGLE_EXPO_CLIENT_ID
+        });
     }
 
     componentDidMount() {
-
         const firebaseConfig = {
             apiKey: "AIzaSyCf1tmxlB1psWda53hTp8Y2OftP3FM9yag",
             authDomain: "fir-1cde2.firebaseapp.com",
@@ -60,11 +37,10 @@ export default class App extends React.Component {
             projectId: "fir-1cde2",
             storageBucket: "fir-1cde2.appspot.com",
             messagingSenderId: "1026090597268",
-            appId: "1:1026090597268:web:e9193de0f49bf2651c6d81"
+            appId: "1:1026090597268:web:f6717d9eae7ddd7e1c6d81"
         };
         const app = initializeApp(firebaseConfig);
         this.fetchState();
-
     }
 
     async fetchState() {
@@ -77,7 +53,6 @@ export default class App extends React.Component {
                     });
                 }
                 else {
-                    console.log('user id: ' + JSON.stringify(user));
                     this.setState({
                         loggedIn: false,
                         isReady: true,
@@ -98,21 +73,31 @@ export default class App extends React.Component {
         if (type === 'success') {
             // Build Firebase credential with the Facebook access token.
             //const facebookAuthProvider = new FacebookAuthProvider();
-
             const credential = FacebookAuthProvider.credential(token);
-            console.log(credential);
             this.signIn(auth, credential);
         }
     }
 
+    loginWithGoogle = async () => {
+        try {
+            const { type, user } = await GoogleSignIn.signInAsync({
+            });
+            if (type === 'success') {
+                const auth = getAuth();
+                const credential = GoogleAuthProvider.credential(user.auth.idToken);
+                this.signIn(auth, credential);
+            }
+        } catch ({ message }) {
+            Alert.alert('Google Login Error: ', message);
+        }
+    };
+
     signIn(auth, credential) {
         signInWithCredential(auth, credential)
             .then(result => {
-                //console.log('result: ' + JSON.stringify(result));
                 const db = getDatabase();
                 fetchSignInMethodsForEmail(auth, result.user.email)
                     .then(signInMethods => {
-                        console.log(signInMethods);
                         if (signInMethods.length == 0) {
                             set(ref(db, 'users/' + result.user.uid), {
                                 username: result.user.displayName,
@@ -147,18 +132,21 @@ export default class App extends React.Component {
                     <View style={{
                         flex: 1, alignItems: 'center', justifyContent: 'center'
                     }}>
-                        <Text>Home Screen</Text>
-                        <Button
-                            onPress={() => this.loginWithFacebook()}
-                            title="Learn More"
-                            color="#841584"
-                            accessibilityLabel="Learn more about this purple button"
-                        />
-                        <GoogleSignIn onReturn={async (id_token) => {
-                            const auth = getAuth();
-                            const credential = GoogleAuthProvider.credential(id_token);
-                            this.signIn(auth, credential);
-                        }} />
+                        <Text>Sign In</Text>
+                        <View>
+                            <Button
+                                onPress={() => this.loginWithFacebook()}
+                                title="Facebook Login"
+                                color="#841584"
+                            />
+                        </View>
+                        <View>
+                            <Button
+                                onPress={() => this.loginWithGoogle()}
+                                title="GoogleLogin"
+                                color="#841584"
+                            />
+                        </View>
                     </View>
                     :
                     <NavigationContainer>
